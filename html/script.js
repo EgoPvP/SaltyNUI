22d59bbd1fa51820d2aef849a3347f1d28ab16f4
Lars H <30929430+ZockerBoysHD@users.noreply.github.com>
2020-12-23 22:10:16 +0100

    Update script.js

--- html/script.js
+++ html/script.js
@@ -1,7 +1,7 @@
 $(function(){
 	window.addEventListener('message', function(event) {
 		if (event.data.action == "toggleWindow"){
-			displayPluginScreen(false);
+			displayPluginScreen(event.data.value);
 		}
 	});
 
