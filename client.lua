e8bc9ad26a3d86f17fdebdbc00dc729f4b87e1f7
Lars H <30929430+ZockerBoysHD@users.noreply.github.com>
2021-12-14 17:22:51 +0100

    added info

--- client.lua
+++ client.lua
@@ -1,3 +1,13 @@
+-- Made by ! der_nqmenl0se#1337 --
+-- Support & more of my Work - https://discord.gg/qRc5Hbb --
+-- Copyright (C) 2020 - EgoPvP.com
+-- This program Is free software: you can redistribute it And/Or modify it under the terms Of the GNU General Public License As published 
+-- by the Free Software Foundation, either version 3 Of the License, Or (at your option) any later version.
+-- This program Is distributed In the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty Of MERCHANTABILITY Or 
+-- FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License For more details.
+-- You should have received a copy Of the GNU General Public License along with this program. If Not, see http://www.gnu.org/licenses/.
+
+
 local ismuted = false
 
 --- SaltyNUI Events ---
