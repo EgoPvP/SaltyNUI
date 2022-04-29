48e7fca1c7d9fa1f6b94d75db465a2b3e133dec8
20maty <102318489+20maty@users.noreply.github.com>
2022-04-29 19:03:38 +0200

    Now the NUI can have two states (hideNUI & showNUI)
    In my case (esx legacy) the toggle thing wasnt working, so i simply created a second event in the js

--- html/script.js
+++ html/script.js
@@ -1,14 +1,10 @@
 $(function(){
 	window.addEventListener('message', function(event) {
-		if (event.data.action == "toggleWindow"){
-		if(event.data.value == "true"){
-			
-			displayPluginScreen(event.data.value);
-			}else{
-			
+		if (event.data.action == "showNUI"){
+			displayPluginScreen(true);
+		}
+		if (event.data.action == "hideNUI"){
 			displayPluginScreen(false);
-			}
-
 		}
 	});
 
